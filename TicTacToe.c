ab4942fca7ee8c54cb92709970efe10d3131f8c3
TMFlach <tmflach@cpp.edu>
2022-12-15 19:59:29 -0800

    Added check for game over state.

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,5 +1,6 @@
 //TicTacToe Game client
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 int TicTacToeGame(int (*)(char *)); //takes a function pointer of whether human player or computer player moves.
@@ -25,7 +26,7 @@ int main() {
         exit = scanf(" %d", &input);
         printf("You entered: %d\n", input);
     } while (exit > 0);
-    printf("Last enterd selection: %d\n", input);
+    printf("Last entered selection: %d\n", input);
     return 0;
 }
 
@@ -50,22 +51,65 @@ void PrintBoard(char * board) {
 int TicTacToeGame(int (*movement)(char*)) {
     char* board = NewBoard();
     //PrintBoard(board);
-    int nextMove = movement(board);
-    board[nextMove] = 'X';
+    while (!GameOver(board)) {
+        int playerMove = PlayerMove(board);
+        if(playerMove < 0) {
+            return 1;//player has chosen to quit.
+        }
+        board[playerMove] = 'O';
+        if(GameOver(board)) {
+            return 0;
+        }
+        int nextMove = movement(board);
+        if(nextMove < 0) {
+            return 1; //player has chosen to quit.
+        }
+        board[nextMove] = 'X';
+    }
     PrintBoard(board);
+    return 0;
 }
 
-int PlayerMove(char * game) {
-    int selection;
+int PlayerMove(char game[]) {
+    int selection = 0;
     do
     {
         PrintBoard(game);
         printf("Make your move\n");
-        int valid = scanf(" %d", &selection);
+        char line[10];
+        scanf("%s", line);
+        //printf("Read %s\n", line);
+        selection = atoi(line);
         selection = selection - 1;
-        if(valid <= 0 || selection < 0 || selection >= 9) {
+        if (selection == -1) { //if atoi failed to read a number, returns 0 which is now -1.
+            return -1; //no number we assume the user wanted to quit.
+        }else if(selection < 0 || selection >= 9) {
             continue;
         }
     } while (game[selection] == 'X' || game[selection] == 'O');
     return selection;
+}
+
+bool GameOver(char board[]) {
+    //check columns
+    for(int i = 0; i < 3; i++) {
+        if(board[i] == board[i+3] && board[i+3] == board[i+6]){
+            return true;
+        } //check rows 
+        else if(board[0 + (3*i)] == board[1 + (3*i)] && board[1 + (3*i)] == board[2 + (3*i)]) {
+            return true;
+        }
+    }
+    //check diagonals
+    if((board[0] == board[4] && board[4] == board[8]) ||
+        board[6] == board[4] && board[4] == board[2]) {
+        return true;
+    }
+    //check to see if the game is tied.
+    for(int i = 0; i < 9; i++) {
+        if(board[i] != 'X' && board[i] != 'O') {
+            return false;
+        }
+    }
+    return true;
 }
