0cd2b9e6f3d517e4711e3b4d08ac07b5f0c695da
TMFlach <tmflach@cpp.edu>
2022-12-15 17:00:10 -0800

    Created prototype game function to test and successfully implement player movement.

--- TicTacToe.c
+++ TicTacToe.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int TicTacToeGame(void); //takes a function pointer of whether human player or computer player moves.
+int TicTacToeGame(int (*)(char *)); //takes a function pointer of whether human player or computer player moves.
 int PlayerMove(char[]);
 int ComputerMove(char[]);
 bool GameOver(char[]); //checks games state to check for game over.
@@ -10,19 +10,19 @@ void PrintBoard(char*);
 char * NewBoard(); //TicTacToe board is represented by 1D array of char.
 
 int main() {
-    int input;
+    int input = 0;
     int exit;
     do
     {
+        if(input == 2) {
+            TicTacToeGame(PlayerMove);
+            
+        }
         printf("Enter your selection:\n");
         printf("1: 1 player game.\n");
         printf("2: 2 player game.\n");
         printf("q: quit\n");
         exit = scanf(" %d", &input);
-        if(input == 1) {
-            char* board = NewBoard();
-            PrintBoard(board);
-        }
         printf("You entered: %d\n", input);
     } while (exit > 0);
     printf("Last enterd selection: %d\n", input);
@@ -45,4 +45,27 @@ void PrintBoard(char * board) {
         }
         printf("\n");
     }
+}
+
+int TicTacToeGame(int (*movement)(char*)) {
+    char* board = NewBoard();
+    //PrintBoard(board);
+    int nextMove = movement(board);
+    board[nextMove] = 'X';
+    PrintBoard(board);
+}
+
+int PlayerMove(char * game) {
+    int selection;
+    do
+    {
+        PrintBoard(game);
+        printf("Make your move\n");
+        int valid = scanf(" %d", &selection);
+        selection = selection - 1;
+        if(valid <= 0 || selection <= 0 || selection >= 9) {
+            continue;
+        }
+    } while (game[selection] == 'X' || game[selection] == 'O');
+    return selection;
 }
